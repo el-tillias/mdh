fb4a9ddee4392eeee1ea93516c2919eec9f48c0d
j <nhzcrypto@gmail.com>
2014-10-02 22:48:12 +0200

    trying to add "try again"

--- dva117/labb4/3.c
+++ dva117/labb4/3.c
@@ -4,6 +4,31 @@
 #include <stddef.h>
 #include <ctype.h>
 
+int again(void) {
+
+    char again;
+
+    while(1) {
+
+        printf("Run again? (y/n): ");
+        scanf(" %c", &again);
+        if (again == 'y') {
+            return 1;
+        }
+        else if (again == 'n') {
+            return 0;
+        }
+        else {
+            printf("\nAnswer with 'y' or 'n'");
+        }
+    }
+
+    return 0;
+
+}
+
+
+
 int convert(char *input, char **wordpointers) {
 
     int i=0, z=0, x=1;
@@ -26,25 +51,37 @@ int convert(char *input, char **wordpointers) {
 
 int main(void) {
 
-    char input[200];
-    char *wordpointers[200];
-    int i, count, z;
+    while(1) {
+        char input[200];
+        char *wordpointers[200];
+        int i, count, z, y;
 
-    printf("Write a word: ");
-    fgets(input, sizeof(input), stdin);
+        printf("Write a word: ");
+        fgets(input, sizeof(input), stdin);
 
-    count = convert(input, wordpointers);
+        count = convert(input, wordpointers);
 
-    for (i=0; i<count; i++) {
-        printf("Word %i (ptr: %p): ", i, &wordpointers[i]);
-        for (z=0; z<strlen(wordpointers[i]); z++) {
-            if (wordpointers[i][z] == '0') {
-                printf("\n");
-                break;
+        for (i=0; i<count; i++) {
+            printf("Word %i (ptr: %p): ", i, &wordpointers[i]);
+            for (z=0; z<strlen(wordpointers[i]); z++) {
+                if (wordpointers[i][z] == '0') {
+                    printf("\n");
+                    break;
+                }
+                else {
+                    printf("%c", wordpointers[i][z]);
+                }
             }
-            else {
-                printf("%c", wordpointers[i][z]);
-            }
+        }
+
+        y = again();
+
+        if (y == 1) {
+            continue;
+
+        }
+        else if (y == 0) {
+            break;
         }
     }
 
