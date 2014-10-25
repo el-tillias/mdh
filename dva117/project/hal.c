e48a70b7421e7d6c18736db81e5f2eea88654b41
j <nhzcrypto@gmail.com>
2014-10-25 22:53:21 +0200

    working http parser

--- dva117/project/hal.c
+++ dva117/project/hal.c
@@ -1,5 +1,3 @@
-/*Required Headers*/
- 
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
@@ -9,44 +7,38 @@
 #include "method.h"
 
 
-struct http_req {
-    char *ip;
-    char *method;
-    char *file;
-    char *filepath;
-    char *http_ver;
-};
+int main(void) {
 
-struct sockaddr_in servaddr;
-
-
-int main(void)
-{
-
-    int listen_fd, comm_fd, i, x, z, m, n, k=0;
-    struct sockaddr_in servaddr;
     char incoming_request[100];
+    int comm_fd, listen_fd;
+    struct sockaddr_in servaddr;
+    struct http_req h;
 
-    listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    listen_fd = socket(AF_INET, SOCK_STREAM, 0); 
     bzero( &servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htons(INADDR_ANY);
-    servaddr.sin_port = htons(22000);
+    servaddr.sin_port = htons(1400);
     bind(listen_fd, (struct sockaddr *) &servaddr, sizeof(servaddr));
     listen(listen_fd, 10);
 
 
-    while (1) {
 
-        int y, i = 0, start_new_session = 0;
+    while(1) {
+
         comm_fd = accept(listen_fd, (struct sockaddr*) NULL, NULL);
 
-        while(1) {
- 
+        while (1) {
+
             bzero(incoming_request, 1000);
             read(comm_fd,incoming_request,1000);
-            parse_http_req(incoming_request);
+            parse_http_req(&h, incoming_request);
 
+            printf("method: %s\n", h.method);
+            printf("filepath: %s\n", h.fullfilepath);
+            printf("http_ver: %s\n", h.http_ver);
         }
     }
+
+    return 0;
 }
--- dva117/project/method.h
+++ dva117/project/method.h
@@ -1,7 +1,17 @@
+struct http_req {
+    char *ip;
+    char *method;
+    char *file;
+    char *path;
+    char *fullfilepath;
+    char *http_ver;
+};  
+
+
 char *skip_character(char **buf, const char *d) {
 
     char *p, *w_begin, *w_end, *d_end;
-
+    
     w_begin = *buf;
     w_end = w_begin + strcspn(w_begin, d);
     d_end = w_end + strspn(w_end, d);
@@ -16,32 +26,19 @@ char *skip_character(char **buf, const char *d) {
 
 }
 
-void parse_http_req(char *http_string) {
 
-    struct http_req {
-        char *ip;
-        char *method;
-        char *file;
-        char *filepath;
-        char *http_ver;
-    };
-
-    struct http_req *n;
-
-    n = (struct http_req *) malloc(sizeof(struct http_req));
+int parse_http_req(struct http_req *n, char *http_string) {
 
     int request_length = strlen(http_string);
-    http_string[request_length-1] = '\0';
+
+    printf("http len: %i\n", request_length);
+    http_string[request_length+2] = '\0';
 
     while (*http_string != '\0' && isspace(* (unsigned char *) http_string)) {
         http_string++;
     }
 
     n->method = skip_character(&http_string, " ");
-    n->filepath = skip_character(&http_string, " ");
+    n->fullfilepath = skip_character(&http_string, " ");
     n->http_ver = skip_character(&http_string, "\r\n");
-
-    printf("method: %s\n", n->method);
-    printf("filepath: %s\n", n->filepath);
-    printf("http_ver: %s\n", n->http_ver);
 }
